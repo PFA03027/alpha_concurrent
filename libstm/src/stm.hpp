2ed7c4fdf588b855d6b97cfb3b3234aa8bdc0647
PFA03027 <PFA03027@nifty.com>
2020-12-20 00:11:42 +0900

    No memory leak, but thread is busy loop wait is happened.

--- libstm/src/stm.hpp
+++ libstm/src/stm.hpp
@@ -10,6 +10,7 @@
 
 #include <cassert>
 #include <atomic>
+#include <thread>
 #include <memory>
 #include <iostream>
 using namespace std;
@@ -36,8 +37,22 @@ public:
 	 */
 	std::shared_ptr<const T> read_value( void ) const
 	{
-		transactional_obj*	p_old_tobj = atomic_p_tobj_.load();
-		return p_old_tobj->read_value();
+		transactional_obj*	p_old_tobj = nullptr;
+		while ( true ) {
+			p_old_tobj = atomic_p_tobj_.load();
+			if(p_old_tobj == nullptr) {
+				std::this_thread::yield();
+				continue;
+			}
+			if ( atomic_p_tobj_.compare_exchange_weak( p_old_tobj, nullptr ) ) {
+				break;
+			}
+			std::this_thread::yield();
+		}
+		// ここに来た時点で、p_old_tobjは、このインスタンスのみが所有している。
+		std::shared_ptr<const T>	sp_ans = p_old_tobj->read_value();
+		atomic_p_tobj_.store( p_old_tobj );
+		return sp_ans;
 	}
 
 	/*!
@@ -52,6 +67,8 @@ public:
 		TFunc modify_func   //!< [in]	this functor is the procedure of modificatio. This will be happened multi-call in case of collision.
 	)
 	{
+#if	0
+		// 単体でのリードモディファイライトの場合は、下記でも動くが、最初から、COMMITEDでよい。
 		state                               expected = state::ACTIVE;
 		std::shared_ptr<std::atomic<state>> sp_atomic_state;
 
@@ -63,18 +80,50 @@ public:
 
 			while ( true ) {
 				p_old_tobj = atomic_p_tobj_.load();
-				auto sp_read_value = p_old_tobj->read_value();
-
-				transactional_obj*	p_new_tobj = new transactional_obj( std::move( sp_read_value ), modify_func( *sp_read_value ), sp_atomic_state );
-
-				if ( atomic_p_tobj_.compare_exchange_weak( p_old_tobj, p_new_tobj ) ) {
-					// How to avoid memory leak of old tobj pointer.
-					break;
-				} else {
-					delete p_new_tobj;
+				if(p_old_tobj == nullptr) {
+					std::this_thread::yield();
+					continue;
 				}
+				if ( atomic_p_tobj_.compare_exchange_weak( p_old_tobj, nullptr ) ) {
+					break;
+				}
+				std::this_thread::yield();
 			}
+			// ここに来た時点で、p_old_tobjは、このインスタンスのみが所有している。
+			auto sp_read_value = p_old_tobj->read_value();
+
+			transactional_obj*	p_new_tobj = new transactional_obj( std::move( sp_read_value ), modify_func( *sp_read_value ), sp_atomic_state );
+
+			atomic_p_tobj_.store( p_new_tobj );
+			delete p_old_tobj;
 		} while ( !sp_atomic_state->compare_exchange_weak( expected, state::COMMITED ) );
+#else
+		std::shared_ptr<std::atomic<state>> sp_atomic_state = std::make_shared<std::atomic<state>>( state::COMMITED );
+
+		transactional_obj*	p_old_tobj = nullptr;
+
+		// これで、メモリリークは発生しないが、yield()している時点でリアルタイム処理には適用できない。
+		// なぜならば、優先度が高い処理が低い処理を踏み越えていけないため、いくら実行権を放棄しても優先度の低い処理に実行権が回らないので（シングルCPUの場合）、意味がない
+		// よって、TSSにしか適用できないが、TSSなら、わざわざロックフリーアルゴリズムを適用するまでもなく、ロック方式でよい。。。
+		while ( true ) {
+			p_old_tobj = atomic_p_tobj_.load();
+			if(p_old_tobj == nullptr) {
+				std::this_thread::yield();
+				continue;
+			}
+			if ( atomic_p_tobj_.compare_exchange_weak( p_old_tobj, nullptr ) ) {
+				break;
+			}
+			std::this_thread::yield();
+		}
+		// ここに来た時点で、p_old_tobjは、このインスタンスのみが所有している。
+		auto sp_read_value = p_old_tobj->read_value();
+
+		transactional_obj*	p_new_tobj = new transactional_obj( std::move( sp_read_value ), modify_func( *sp_read_value ), sp_atomic_state );
+
+		atomic_p_tobj_.store( p_new_tobj );
+		delete p_old_tobj;
+#endif
 	}
 
 #if	0
@@ -139,7 +188,7 @@ private:
 		std::shared_ptr<std::atomic<state>> const sp_owner_;
 	};
 
-	std::atomic<transactional_obj*> atomic_p_tobj_;   // need to be accessed by atomic style.
+	mutable std::atomic<transactional_obj*> atomic_p_tobj_;   // need to be accessed by atomic style.
 };
 
 }   // namespace concurrent
