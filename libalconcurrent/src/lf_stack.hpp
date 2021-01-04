ed93530d82f28392b6ae2900f4fd7cd30e27ca00
PFA03027 <PFA03027@nifty.com>
2021-01-04 18:52:32 +0900

    Add header file only.

---
+++ libalconcurrent/src/lf_stack.hpp
@@ -0,0 +1,18 @@
+/*!
+ * @file	lf_stack.hpp
+ * @brief
+ * @author	Teruaki Ata
+ * @date	Created on 2021/01/04
+ * @details
+ *
+ * Copyright (C) 2021 by Teruaki Ata <PFA03027@nifty.com>
+ */
+
+#ifndef SRC_LF_STACK_HPP_
+#define SRC_LF_STACK_HPP_
+
+
+
+
+
+#endif /* SRC_LF_STACK_HPP_ */
