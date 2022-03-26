6c8d2d7587d29fd74c3bf7cc77cd618582c157a5
PFA03027 <PFA03027@nifty.com>
2022-03-27 00:06:30 +0900

    feat: add all sanitizer test script on linux

---
+++ make_linux_full_sanitizer.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo $@
+
+# If you would like to build with target or environment specific configuration:
+# 1. please prepare XXXX.cmake that includes build options
+# 2. provide file information of XXXX.cmake like below to CMakeLists.txt
+#    with -D option like below
+#        $ cmake -D BUILD_TARGET=`pwd`/../XXXX
+#    common.cmake is default configurations
+# 
+BUILDTARGET=common
+
+# Debug or Release or ...
+#BUILDTYPE=Debug
+BUILDTYPE=Release
+
+for i in {1..14}
+do
+	rm -fr build
+	mkdir -p build
+	cd build
+	cmake -DCMAKE_BUILD_TYPE=${BUILDTYPE} -DBUILD_TARGET=${BUILDTARGET} -DSANITIZER_TYPE=$1 -G "Unix Makefiles" ../
+	cmake --build . -j 8 -v --target build-test
+	echo $i / 14.
+	cmake --build . -j 8 -v --target test
+	result=$?
+	if [ "$result" = "0" ]; then
+		echo $i is OK. result=$result
+	else
+		echo $i is FAIL. result=$result
+		exit $result
+	fi
+	cd ..
+done
