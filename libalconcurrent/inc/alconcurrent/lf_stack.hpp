7316b26fd915ed8bb6a471ae8fc42be8a9060316
Alpha <PFA03027@nifty.com>
2024-05-04 02:51:05 +0900

    refactor: prepare to support gmem_allocate

--- libalconcurrent/inc/alconcurrent/lf_stack.hpp
+++ libalconcurrent/inc/alconcurrent/lf_stack.hpp
@@ -388,7 +388,7 @@ public:
 	void push( T& v_arg )
 	{
 		lilo_node* p_expected = hph_head_.load( std::memory_order_acquire );
-		lilo_node* p_new_node = new lilo_node { v_arg, hazard_ptr_handler<lilo_node>( p_expected ) };
+		lilo_node* p_new_node = new lilo_node { hazard_ptr_handler<lilo_node>( p_expected ), v_arg };
 		while ( !hph_head_.compare_exchange_weak( p_expected, p_new_node, std::memory_order_release, std::memory_order_relaxed ) ) {
 			p_new_node->hph_next_.store( p_expected, std::memory_order_release );
 		}
@@ -397,7 +397,7 @@ public:
 	void push( T&& v_arg )
 	{
 		lilo_node* p_expected = hph_head_.load( std::memory_order_acquire );
-		lilo_node* p_new_node = new lilo_node { std::move( v_arg ), hazard_ptr_handler<lilo_node>( p_expected ) };
+		lilo_node* p_new_node = new lilo_node { hazard_ptr_handler<lilo_node>( p_expected ), std::move( v_arg ) };
 		while ( !hph_head_.compare_exchange_weak( p_expected, p_new_node, std::memory_order_release, std::memory_order_relaxed ) ) {
 			p_new_node->hph_next_.store( p_expected, std::memory_order_release );
 		}
@@ -429,8 +429,155 @@ public:
 
 private:
 	struct lilo_node {
-		value_type                    v_;
 		hazard_ptr_handler<lilo_node> hph_next_;
+		value_type                    v_;
+
+#ifdef ALCONCURRENT_CONF_USE_MALLOC_ALLWAYS_FOR_DEBUG_WITH_SANITIZER
+#else
+#if 0   // too slow...
+#if __cpp_aligned_new
+		ALCC_INTERNAL_NODISCARD_ATTR void* operator new( std::size_t size, std::align_val_t alignment )   // possible throw std::bad_alloc, from C++17
+		{
+			void* p_ans = gmem_allocate( size, static_cast<size_t>( alignment ) );
+			if ( p_ans == nullptr ) {
+				throw std::bad_alloc();
+			}
+			return p_ans;
+		}
+		ALCC_INTERNAL_NODISCARD_ATTR void* operator new( std::size_t size, std::align_val_t alignment, const std::nothrow_t& ) noexcept   // possible return nullptr, instead of throwing exception, from C++17
+		{
+			return gmem_allocate( size, static_cast<size_t>( alignment ) );
+		}
+		ALCC_INTERNAL_NODISCARD_ATTR void* operator new[]( std::size_t size, std::align_val_t alignment )   // possible throw std::bad_alloc, from C++17
+		{
+			void* p_ans = gmem_allocate( size, static_cast<size_t>( alignment ) );
+			if ( p_ans == nullptr ) {
+				throw std::bad_alloc();
+			}
+			return p_ans;
+		}
+		ALCC_INTERNAL_NODISCARD_ATTR void* operator new[]( std::size_t size, std::align_val_t alignment, const std::nothrow_t& ) noexcept   // possible return nullptr, instead of throwing exception, from C++17
+		{
+			return gmem_allocate( size, static_cast<size_t>( alignment ) );
+		}
+#endif
+		ALCC_INTERNAL_NODISCARD_ATTR void* operator new( std::size_t size )   // possible throw std::bad_alloc, from C++11
+		{
+			void* p_ans = gmem_allocate( size );
+			if ( p_ans == nullptr ) {
+				throw std::bad_alloc();
+			}
+			return p_ans;
+		}
+		ALCC_INTERNAL_NODISCARD_ATTR void* operator new( std::size_t size, const std::nothrow_t& ) noexcept   // possible return nullptr, instead of throwing exception, from C++11
+		{
+			return gmem_allocate( size );
+		}
+		ALCC_INTERNAL_NODISCARD_ATTR void* operator new[]( std::size_t size )   // possible throw std::bad_alloc, from C++11
+		{
+			void* p_ans = gmem_allocate( size );
+			if ( p_ans == nullptr ) {
+				throw std::bad_alloc();
+			}
+			return p_ans;
+		}
+		ALCC_INTERNAL_NODISCARD_ATTR void* operator new[]( std::size_t size, const std::nothrow_t& ) noexcept   // possible return nullptr, instead of throwing exception, from C++11
+		{
+			return gmem_allocate( size );
+		}
+
+		ALCC_INTERNAL_NODISCARD_ATTR void* operator new( std::size_t size, void* ptr ) noexcept   // placement new, from C++11
+		{
+			return ptr;
+		}
+		ALCC_INTERNAL_NODISCARD_ATTR void* operator new[]( std::size_t size, void* ptr ) noexcept   // placement new for array, from C++11
+		{
+			return ptr;
+		}
+
+#if __cpp_aligned_new
+		void operator delete( void* ptr, std::align_val_t alignment ) noexcept   // from C++17, and no sized deallocation support(in case of using clang without -fsized-deallocation)
+		{
+			gmem_deallocate( ptr );
+		}
+		void operator delete[]( void* ptr, std::align_val_t alignment ) noexcept   // from C++17, and no sized deallocation support(in case of using clang without -fsized-deallocation)
+		{
+			gmem_deallocate( ptr );
+		}
+
+		void operator delete( void* ptr, std::size_t size, std::align_val_t alignment ) noexcept   // from C++17
+		{
+			gmem_deallocate( ptr );
+		}
+		void operator delete[]( void* ptr, std::size_t size, std::align_val_t alignment ) noexcept   // from C++17
+		{
+			gmem_deallocate( ptr );
+		}
+
+		void operator delete( void* ptr, std::align_val_t alignment, const std::nothrow_t& ) noexcept   // from C++17, and no sized deallocation support(in case of using clang without -fsized-deallocation)
+		{
+			gmem_deallocate( ptr );
+		}
+		void operator delete[]( void* ptr, std::align_val_t alignment, const std::nothrow_t& ) noexcept   // from C++17, and no sized deallocation support(in case of using clang without -fsized-deallocation)
+		{
+			gmem_deallocate( ptr );
+		}
+
+		void operator delete( void* ptr, std::size_t size, std::align_val_t alignment, const std::nothrow_t& ) noexcept   // from C++17
+		{
+			gmem_deallocate( ptr );
+		}
+		void operator delete[]( void* ptr, std::size_t size, std::align_val_t alignment, const std::nothrow_t& ) noexcept   // from C++17
+		{
+			gmem_deallocate( ptr );
+		}
+#endif
+		void operator delete( void* ptr ) noexcept   // from C++11
+		{
+			gmem_deallocate( ptr );
+		}
+		void operator delete[]( void* ptr ) noexcept   // from C++11
+		{
+			gmem_deallocate( ptr );
+		}
+
+		void operator delete( void* ptr, std::size_t size ) noexcept   // from C++14
+		{
+			gmem_deallocate( ptr );
+		}
+		void operator delete[]( void* ptr, std::size_t size ) noexcept   // from C++14
+		{
+			gmem_deallocate( ptr );
+		}
+
+		void operator delete( void* ptr, const std::nothrow_t& ) noexcept   // from C++11
+		{
+			gmem_deallocate( ptr );
+		}
+		void operator delete[]( void* ptr, const std::nothrow_t& ) noexcept   // from C++11
+		{
+			gmem_deallocate( ptr );
+		}
+
+		void operator delete( void* ptr, std::size_t size, const std::nothrow_t& ) noexcept   // from C++14
+		{
+			gmem_deallocate( ptr );
+		}
+		void operator delete[]( void* ptr, std::size_t size, const std::nothrow_t& ) noexcept   // from C++14
+		{
+			gmem_deallocate( ptr );
+		}
+
+		void operator delete( void* ptr, void* ) noexcept   // delete for area that is initialized by placement new.
+		{
+			// nothing to do
+		}
+		void operator delete[]( void* ptr, void* ) noexcept   // delete for area that is initialized by placement new.
+		{
+			// nothing to do
+		}
+#endif
+#endif
 	};
 
 	lilo_node* pop_impl( void )
