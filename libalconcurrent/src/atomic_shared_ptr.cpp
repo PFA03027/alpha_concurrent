13acf5ea473b4d85c1c882ce56968bf27a732aab
Alpha <PFA03027@nifty.com>
2025-02-15 16:13:15 +0900

    feat: new operator of control block by gmem_allocator()

--- libalconcurrent/src/atomic_shared_ptr.cpp
+++ libalconcurrent/src/atomic_shared_ptr.cpp
@@ -10,6 +10,7 @@
  */
 
 #include "alconcurrent/experiment/internal/atomic_shared_ptr.hpp"
+#include "alconcurrent/lf_mem_alloc.hpp"
 
 namespace alpha {
 namespace concurrent {
@@ -20,6 +21,150 @@ void control_block_base::retire( control_block_base* p )
 {
 	delete p;
 }
+
+#ifdef ALCONCURRENT_CONF_USE_MALLOC_ALLWAYS_FOR_DEBUG_WITH_SANITIZER
+#else
+ALCC_INTERNAL_NODISCARD_ATTR void* control_block_base::operator new( std::size_t size )   // possible throw std::bad_alloc, from C++11
+{
+	void* p_ans = gmem_allocate( size );
+	if ( p_ans == nullptr ) {
+		throw std::bad_alloc();
+	}
+	return p_ans;
+}
+ALCC_INTERNAL_NODISCARD_ATTR void* control_block_base::operator new( std::size_t size, const std::nothrow_t& ) noexcept   // possible return nullptr, instead of throwing exception, from C++11
+{
+	return gmem_allocate( size );
+}
+ALCC_INTERNAL_NODISCARD_ATTR void* control_block_base::operator new[]( std::size_t size )   // possible throw std::bad_alloc, from C++11
+{
+	void* p_ans = gmem_allocate( size );
+	if ( p_ans == nullptr ) {
+		throw std::bad_alloc();
+	}
+	return p_ans;
+}
+ALCC_INTERNAL_NODISCARD_ATTR void* control_block_base::operator new[]( std::size_t size, const std::nothrow_t& ) noexcept   // possible return nullptr, instead of throwing exception, from C++11
+{
+	return gmem_allocate( size );
+}
+
+ALCC_INTERNAL_NODISCARD_ATTR void* control_block_base::operator new( std::size_t size, void* ptr ) noexcept   // placement new, from C++11
+{
+	return ptr;
+}
+ALCC_INTERNAL_NODISCARD_ATTR void* control_block_base::operator new[]( std::size_t size, void* ptr ) noexcept   // placement new for array, from C++11
+{
+	return ptr;
+}
+
+void control_block_base::operator delete( void* ptr ) noexcept   // from C++11
+{
+	gmem_deallocate( ptr );
+}
+void control_block_base::operator delete[]( void* ptr ) noexcept   // from C++11
+{
+	gmem_deallocate( ptr );
+}
+
+void control_block_base::operator delete( void* ptr, std::size_t size ) noexcept   // from C++14
+{
+	gmem_deallocate( ptr );
+}
+void control_block_base::operator delete[]( void* ptr, std::size_t size ) noexcept   // from C++14
+{
+	gmem_deallocate( ptr );
+}
+
+void control_block_base::operator delete( void* ptr, const std::nothrow_t& ) noexcept   // from C++11
+{
+	gmem_deallocate( ptr );
+}
+void control_block_base::operator delete[]( void* ptr, const std::nothrow_t& ) noexcept   // from C++11
+{
+	gmem_deallocate( ptr );
+}
+
+void control_block_base::operator delete( void* ptr, std::size_t size, const std::nothrow_t& ) noexcept   // from C++14
+{
+	gmem_deallocate( ptr );
+}
+void control_block_base::operator delete[]( void* ptr, std::size_t size, const std::nothrow_t& ) noexcept   // from C++14
+{
+	gmem_deallocate( ptr );
+}
+
+void control_block_base::operator delete( void* ptr, void* ) noexcept   // delete for area that is initialized by placement new.
+{
+	// nothing to do
+}
+void control_block_base::operator delete[]( void* ptr, void* ) noexcept   // delete for area that is initialized by placement new.
+{
+	// nothing to do
+}
+
+#if __cpp_aligned_new >= 201606
+ALCC_INTERNAL_NODISCARD_ATTR void* control_block_base::operator new( std::size_t size, std::align_val_t alignment )   // possible throw std::bad_alloc, from C++17
+{
+	void* p_ans = gmem_allocate( size, static_cast<size_t>( alignment ) );
+	if ( p_ans == nullptr ) {
+		throw std::bad_alloc();
+	}
+	return p_ans;
+}
+ALCC_INTERNAL_NODISCARD_ATTR void* control_block_base::operator new( std::size_t size, std::align_val_t alignment, const std::nothrow_t& ) noexcept   // possible return nullptr, instead of throwing exception, from C++17
+{
+	return gmem_allocate( size, static_cast<size_t>( alignment ) );
+}
+ALCC_INTERNAL_NODISCARD_ATTR void* control_block_base::operator new[]( std::size_t size, std::align_val_t alignment )   // possible throw std::bad_alloc, from C++17
+{
+	void* p_ans = gmem_allocate( size, static_cast<size_t>( alignment ) );
+	if ( p_ans == nullptr ) {
+		throw std::bad_alloc();
+	}
+	return p_ans;
+}
+ALCC_INTERNAL_NODISCARD_ATTR void* control_block_base::operator new[]( std::size_t size, std::align_val_t alignment, const std::nothrow_t& ) noexcept   // possible return nullptr, instead of throwing exception, from C++17
+{
+	return gmem_allocate( size, static_cast<size_t>( alignment ) );
+}
+void control_block_base::operator delete( void* ptr, std::align_val_t alignment ) noexcept   // from C++17, and no sized deallocation support(in case of using clang without -fsized-deallocation)
+{
+	gmem_deallocate( ptr );
+}
+void control_block_base::operator delete[]( void* ptr, std::align_val_t alignment ) noexcept   // from C++17, and no sized deallocation support(in case of using clang without -fsized-deallocation)
+{
+	gmem_deallocate( ptr );
+}
+
+void control_block_base::operator delete( void* ptr, std::size_t size, std::align_val_t alignment ) noexcept   // from C++17
+{
+	gmem_deallocate( ptr );
+}
+void control_block_base::operator delete[]( void* ptr, std::size_t size, std::align_val_t alignment ) noexcept   // from C++17
+{
+	gmem_deallocate( ptr );
+}
+
+void control_block_base::operator delete( void* ptr, std::align_val_t alignment, const std::nothrow_t& ) noexcept   // from C++17, and no sized deallocation support(in case of using clang without -fsized-deallocation)
+{
+	gmem_deallocate( ptr );
+}
+void control_block_base::operator delete[]( void* ptr, std::align_val_t alignment, const std::nothrow_t& ) noexcept   // from C++17, and no sized deallocation support(in case of using clang without -fsized-deallocation)
+{
+	gmem_deallocate( ptr );
+}
+
+void control_block_base::operator delete( void* ptr, std::size_t size, std::align_val_t alignment, const std::nothrow_t& ) noexcept   // from C++17
+{
+	gmem_deallocate( ptr );
+}
+void control_block_base::operator delete[]( void* ptr, std::size_t size, std::align_val_t alignment, const std::nothrow_t& ) noexcept   // from C++17
+{
+	gmem_deallocate( ptr );
+}
+#endif
+#endif
 
 }   // namespace internal
 }   // namespace concurrent
